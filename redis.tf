ff40546a8554d6499e6a10a5da62d0aa6a7c83f5
Amrut Anubhav <anubhavamrut@gmail.com>
2023-04-07 12:05:24 +0530

    updating

--- redis.tf
+++ redis.tf
@@ -12,18 +12,18 @@
 #   port                 = 6379
 # }
 
-##creates a parameter group
-resource "aws_elasticache_parameter_group" "redis-pg" {
-  name   = "roboshop-${var.ENV}-redis-pg"
-  family = "redis6.x"
-}
-
-# #creates subnet group
-# resource "aws_elasticache_subnet_group" "redis-sg" {
-#   name       = "roboshop-${var.ENV}-redis-sg"
-#   subnet_ids = data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNET_IDS
- 
+# ##creates a parameter group
+# resource "aws_elasticache_parameter_group" "redis-pg" {
+#   name   = "roboshop-${var.ENV}-redis-pg"
+#   family = "redis6.x"
 # }
+
+#creates subnet group
+resource "aws_elasticache_subnet_group" "redis-sg" {
+  name       = "roboshop-${var.ENV}-redis-sg"
+  subnet_ids = data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNET_IDS
+ 
+}
 
 resource "aws_elasticache_parameter_group" "redis-pg" {
   name   = "roboshop-${var.ENV}-redis-pg"
