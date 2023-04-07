a9f1d21d98aeee5a280b02bcc4a9e6747088b6ef
Amrut Anubhav <anubhavamrut@gmail.com>
2023-04-07 12:12:01 +0530

    updating

--- redis.tf
+++ redis.tf
@@ -5,7 +5,7 @@ resource "aws_elasticache_cluster" "redis" {
   engine               = "redis"
   node_type            = "cache.t3.micro"
   num_cache_nodes      = 1
-  # parameter_group_name = aws_elasticache_parameter_group.redis-pg.name
+  parameter_group_name = aws_elasticache_parameter_group.redis-pg.id
   engine_version       = "6.x"
   security_group_ids   = [aws_security_group.allow_redis.id]
   subnet_group_name    = aws_elasticache_subnet_group.redis-sg.name
